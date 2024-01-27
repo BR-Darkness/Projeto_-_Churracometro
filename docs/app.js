0d228c0457ce9474026f42800a5836ab9130965e
BR-Darkness <BRvitorgalindo@gmail.com>
2024-01-27 07:13:41 -0300

    [docs, feat, fix, refactor] funções criar, calcular, atualizar, deletar e retornar Tabela

--- docs/app.js
+++ docs/app.js
@@ -4,6 +4,8 @@ function theme()
     (localStorage.getItem("theme") === "dark") ? localStorage.setItem("theme", "light") : localStorage.setItem("theme", "dark");
 }
 
+if (localStorage.getItem("theme") === "dark") document.body.classList.toggle("dark-theme");
+
 let prevScrollpos = window.scrollY;
 window.onscroll = () => {
     let currentScrollPos = window.scrollY;
@@ -16,94 +18,82 @@ if (localStorage.getItem("theme") === "dark") document.body.classList.toggle("da
 /* ----- ----- ----- ----- ----- */
 
 function obterDataAtualFormatada() {
-    const dataAtual = new Date();
-    const ano = dataAtual.getFullYear();
+    let dataAtual = new Date();
+    let ano = dataAtual.getFullYear();
     let mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0');
     let dia = dataAtual.getDate().toString().padStart(2, '0');
 
-    return `${ano}-${mes}-${dia}`;
+    return `${dia}-${mes}-${ano}`;
 }
 
-// async function fetchData(url) {
+/* ----- ----- Funções Churrasco: ----- ----- */
 
-//     const card = document.createElement('div');
-//     const response = await fetch(url);
-//     const data = await response.json();
+function calcularChurrasco() {
+    let qtdCriancasElement  = document.getElementById("qtd_Criancas");
+    let qtdMulheresElement  = document.getElementById("qtd_Mulheres");
+    let qtdHomensElement    = document.getElementById("qtd_Homens");
 
-//     card.innerHTML =
-//         `<div class="card">
-//             <h3>Data: ${data[0].data}</h3>
-//             <p>Qtd. de Pessoas: ${data[0].qtd_homens + data[0].qtd_mulheres + data[0].qtd_criancas}</p>
-//             <p>Carne (KG): ${data[0].carne_kg}</p>
-//             <p>Pão de Alho: ${data[0].pao_de_alho}</p>
-//             <p>Carvão (KG): ${data[0].carvao_kg}</p>
-//             <p>Refri (L): ${data[0].refri_l}</p>
-//             <p>Cerveja (L): ${data[0].cerveja_l}</p>
-//         </div>
-//         `;
-//     document.getElementById('Teste').appendChild(card);
-// }
+    let qtd_criancas  = Number(qtdCriancasElement.value);
+    let qtd_mulheres  = Number(qtdMulheresElement.value);
+    let qtd_homens    = Number(qtdHomensElement.value);
 
-async function fetchListaChurrascos() {
+    let qtd_pessoas   = qtd_homens + qtd_mulheres + qtd_criancas;
+
+    if (qtd_pessoas == 0) return false;
+
+    const dadosChurrasco = {
+        data:           obterDataAtualFormatada(),
+        qtd_homens:     qtd_homens,
+        qtd_mulheres:   qtd_mulheres,
+        qtd_criancas:   qtd_criancas,
+        carne_kg:       (qtd_homens * 0.4 + qtd_mulheres * 0.32 + qtd_criancas * 0.2).toFixed(1).replace(".", ","),
+        carvao_kg:      (qtd_homens * 1 + qtd_mulheres * 1 + qtd_criancas * 1),
+        pao_de_alho:    (qtd_homens * 2 + qtd_mulheres * 2 + qtd_criancas * 1),
+        refri_l:        (qtd_pessoas <= 5) ? 2 : Math.ceil(qtd_pessoas / 5),
+        cerveja_l:      ((qtd_homens * 3 + qtd_mulheres * 3 + qtd_criancas * 0) * 0.6).toFixed(1).replace(".", ",") 
+        }
+    
+    return dadosChurrasco;
+}
+
+async function criarChurrasco() {
+
+    if (calcularChurrasco() == false) return;
+
     try {
-        const response = await fetch('http://localhost:3000/churrascos');
-        const churrascos = await response.json();
-        const tbody = document.getElementById('churrascoData');
-        churrascos.forEach(churrasco => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${churrasco.data}</td>
-                <td>${churrasco.qtd_homens + churrasco.qtd_mulheres + churrasco.qtd_criancas}</td>
-                <td>${churrasco.carne_kg}</td>
-                <td>${churrasco.pao_de_alho}</td>
-                <td>${churrasco.carvao_kg}</td>
-                <td>${churrasco.refri_l}</td>
-                <td>${churrasco.cerveja_l}</td>
-                <td class="table-actions">
-                    <button class="edit material-symbols-outlined" onclick="editChurrasco('${churrasco.id}')">edit</button>
-                    <button class="delete material-symbols-outlined" onclick="deleteChurrasco('${churrasco.id}')">delete</button>
-                </td>`;
-            tbody.appendChild(row);
+        await fetch('http://localhost:3000/churrascos/', {
+            method: 'POST',
+            body: JSON.stringify(calcularChurrasco())
         });
+        window.location.href = "../index.html";
     } catch (error) {
-        console.error('Erro ao buscar dados da API:', error);
+        console.warn('Erro ao subir dados para à API.');
+        console.error('Erro ao subir dados para à API:', error);
     }
 }
 
-fetchListaChurrascos()
-
-async function novoChurrasco() {
-    let qtdCriancas = document.getElementById("qtdCriancas");
-    let qtdHomens = document.getElementById("qtdHomens");
-    let qtdMulheres = document.getElementById("qtdMulheres");
-        const novoChurrasco = {
-            data: obterDataAtualFormatada(),
-            qtd_homens: Number(qtdHomens.value),
-            qtd_mulheres: Number(qtdMulheres.value),
-            qtd_criancas: Number(qtdCriancas.value),
-            carne_kg: Number(qtdHomens.value) * 0.4 + Number(qtdMulheres.value) * 0.32 + Number(qtdCriancas.value) * 0.2,
-            pao_de_alho: Number(qtdHomens.value) * 2 + Number(qtdMulheres.value) * 2 + Number(qtdCriancas.value) * 1,
-            carvao_kg: Number(qtdHomens.value) * 1 + Number(qtdMulheres.value) * 1 + Number(qtdCriancas.value) * 1,
-            refri_l: ((Number(qtdHomens.value) + Number(qtdMulheres.value) + Number(qtdCriancas.value)) < 5) ? 2 : Math.ceil(((Number(qtdHomens.value) + Number(qtdMulheres.value) + Number(qtdCriancas.value))/5)),
-            cerveja_l: (Number(qtdHomens.value) * 3 + Number(qtdMulheres.value) * 3 + Number(qtdCriancas.value) * 0) * 0.6 
-        }
-    
-        await fetch('http://localhost:3000/churrascos/', {
-            method: 'POST',
-            body: JSON.stringify(novoChurrasco)
-        });
-    
-        window.location.href = "../index.html";
-}
-
-// fetchData("http://localhost:3000/churrascos");
-
-async function deleteChurrasco(id) {
+async function deletarChurrasco(id) {
     try {
         await fetch(`http://localhost:3000/churrascos/${id}`, {
             method: 'DELETE',
         });
     } catch (error) {
+        console.warn('Erro ao deletar churrasco.')
         console.error('Erro ao deletar churrasco:', error);
     }
+}
+
+async function atualizarChurrasco(id) {
+    
+    if (calcularChurrasco() == false) return;
+
+   try {
+        await fetch(`http://localhost:3000/churrascos/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(calcularChurrasco())
+        });
+   } catch (error) {
+        console.warn('Erro ao buscar dados do churrasco, por favor verifique se à ID solicitada esta correta.')
+        console.error('Erro ao buscar dados do churrasco', error);
+   }
 }
